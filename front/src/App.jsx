2a945831510174d5e7aa6ce2bc89d77d37de96e1
piilll <spchoi1998@naver.com>
2024-06-22 14:36:02 +0900

    [SP]Kakao map initialize

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import KakaoMap from './components/KakaoMap'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div style={{ width: '100%', height: '100%' }}>
+      <KakaoMap />
+    </div>
   )
 }
 
